d82617dd1c35a6c1fd4d0aa56d3f18f37898615d
Anshpreet Kaur <anshpreetlayal@outlook.com>
2024-04-22 19:04:41 -0400

    added code

--- airflow/dags/elt_dag.py
+++ airflow/dags/elt_dag.py
@@ -38,7 +38,26 @@ t1 = PythonOperator(
     dag=dag
 )
 
-t1 = DockerOperator(
+t2 = DockerOperator(
     task_id="dbt_run",
-    image='ghcr'
+    image='ghcr.io/dbt-labs/dbt-postgres:1.4.7',
+    command=[
+        "run",
+        "--profiles-dir",
+        "/root"
+        "--project-dir",
+        "/dbt"
+    ],
+    auto_remove=True,
+    docker_url="unix://var/run/docker.sock",
+    network_mode="bridge",
+    mounts=[
+        Mount(source='/Users/anshp/IdeaProjects/elt/custom_postgres',
+               target='/dbt', type='bind'),
+        Mount(source='/Users/anshp/.dbt',
+               target='/root', type='bind'),
+    ],
+    dag=dag
 )
+
+t1 >> t2
